a69417ada81485693a6ad3c8ebe5309a75dd49f5
gamal2018 <eng.gamal.a.abdelsater@gmail.com>
2018-07-11 18:41:16 -0700

    News App Stage 2

--- app/src/main/java/com/example/hamza/NewsFeedApp/ArticleActivity.java
+++ app/src/main/java/com/example/hamza/NewsFeedApp/ArticleActivity.java
@@ -4,11 +4,15 @@ package com.example.hamza.NewsFeedApp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -24,15 +28,59 @@ public class ArticleActivity extends AppCompatActivity implements LoaderCallback
 
     public static final String LOG_TAG = ArticleActivity.class.getName();
     private static final String THEGUARDIAN_REQUEST_URL =
-            "https://content.guardianapis.com/search?&show-tags=contributor&api-key=ce2a9354-bf55-4f0a-bf37-25754f252eb6";
+            "https://content.guardianapis.com/search?show-tags=contributor";
     private ArticleAdapter mAdapter;
     private static final int ARTICLE_LOADER_ID = 1;
     private TextView mEmptyStateTextView;
 
+
+
+
+
     @Override
+    // onCreateLoader instantiates and returns a new Loader for the given ID
     public Loader<List<Article>> onCreateLoader(int i, Bundle bundle) {
-        // Create a new loader for the given URL
-        return new ArticleLoader(this, THEGUARDIAN_REQUEST_URL);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // getString retrieves a String value from the preferences.
+        // The second parameter is the default value for this preference.
+        String keyValue = sharedPrefs.getString(
+                getString(R.string.settings_key_value),
+                getString(R.string.settings_key_value_default));
+        String orderBy  = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+        String section  = sharedPrefs.getString(
+                getString(R.string.settings_section_key),
+                getString(R.string.settings_section_default)
+        );
+        String useDate  = sharedPrefs.getString(
+                getString(R.string.settings_use_date_key),
+                getString(R.string.settings_use_date_default)
+        );
+
+        String fromDate  = sharedPrefs.getString(
+                getString(R.string.settings_from_date_key),
+                getString(R.string.settings_from_date_default)
+        );
+
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(THEGUARDIAN_REQUEST_URL);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value.
+        uriBuilder.appendQueryParameter("q", section);
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+        uriBuilder.appendQueryParameter("use-date", useDate);
+        uriBuilder.appendQueryParameter("from-date",fromDate);
+        uriBuilder.appendQueryParameter("api-key", keyValue);
+
+        return new ArticleLoader(this, uriBuilder.toString());
+
     }
 
     @Override
@@ -113,4 +161,22 @@ public class ArticleActivity extends AppCompatActivity implements LoaderCallback
         }
     }
 
+    @Override
+    // This method initialize the contents of the Activity's options menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    // This method is called whenever an item in the options menu is selected.
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
